b9b60b674de631a32e8083d9c1509b79af57bb06
Peter Hamilton <peter@inspiredpixel.net>
2011-09-11 23:27:34 +0100

    Made the api key be used correctly

--- lib/fiftyfifty.rb
+++ lib/fiftyfifty.rb
@@ -2,10 +2,12 @@ require 'httparty'
 
 class FiftyFifty
   include HTTParty
-  base_uri "5050.gd/api/#{@api_key}/"
+  
+  debug_output $stdout
   
   def initialize(api_key)
      @api_key = api_key
+     self.class.base_uri "5050.gd/api/#{@api_key}"
   end
   
   def statistics
